288ffd137e7216bf359d3aeaac843b92cd3e473d
OliverPérez <oliverperez.e@gmail.com>
2019-01-21 23:26:53 -0600

    Implements UI custom controls in a dummy view controller.

--- SumAR/Controllers/RangeSliderViewController.swift
+++ SumAR/Controllers/RangeSliderViewController.swift
@@ -1,39 +1,69 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-  let rangeSlider = RangeSlider(frame: .zero)
+  let upDownSlider = RangeSlider(frame: .zero)
+  let rudderSlider = RangeSlider(frame: .zero)
+  let engineSlider = RangeSlider(frame: .zero)
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    view.addSubview(rangeSlider)
-    rangeSlider.addTarget(self, action: #selector(rangeSliderValueChanged(_:)),
+    view.addSubview(upDownSlider)
+    view.addSubview(rudderSlider)
+    view.addSubview(engineSlider)
+
+    upDownSlider.addTarget(self, action: #selector(upDownSliderValueChanged(_:)),
                           for: .valueChanged)
+    
+    rudderSlider.addTarget(self, action: #selector(rudderSliderValueChanged(_:)), for: .valueChanged)
+    
+     engineSlider.addTarget(self, action: #selector(engineSliderValueChanged(_:)), for: .valueChanged)
+    
     let time = DispatchTime.now() + 1
-    let width = view.bounds.width / 2
-    let margin: CGFloat = 30
-    let height: CGFloat = 60
     
     DispatchQueue.main.asyncAfter(deadline: time) {
-      self.rangeSlider.trackHighlightTintColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
-      self.rangeSlider.thumbImage = #imageLiteral(resourceName: "RectThumb")
-      self.rangeSlider.highlightedThumbImage = #imageLiteral(resourceName: "HighlightedRect")
-        self.rangeSlider.transform = self.rangeSlider.transform.rotated(by: CGFloat(-Float.pi/2))
-  
+      self.upDownSlider.trackHighlightTintColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+      self.upDownSlider.thumbImage = #imageLiteral(resourceName: "RectThumb")
+      self.upDownSlider.highlightedThumbImage = #imageLiteral(resourceName: "HighlightedRect")
+      
+      self.rudderSlider.trackHighlightTintColor = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
+      self.rudderSlider.thumbImage = #imageLiteral(resourceName: "RectThumb")
+      self.rudderSlider.highlightedThumbImage = #imageLiteral(resourceName: "HighlightedRect")
         
-        self.rangeSlider.frame = CGRect(x: 0, y: 0,
-                                   width: height, height: width - margin)
-        self.rangeSlider.center = self.view.center
-
+      self.engineSlider.trackHighlightTintColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+      self.engineSlider.thumbImage = #imageLiteral(resourceName: "RectThumb")
+      self.engineSlider.highlightedThumbImage = #imageLiteral(resourceName: "HighlightedRect")
+    
     }
   }
   
   override func viewDidLayoutSubviews() {
+    let width: CGFloat = view.frame.width / 10
+    let height: CGFloat = view.frame.height / 3
     
+    upDownSlider.frame = CGRect(x: 0, y: 0, width: height, height: width)
+    upDownSlider.center = CGPoint(x: view.frame.width - 60, y: view.frame.height - height/2 - 20)
+    upDownSlider.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/2)
+    
+    engineSlider.frame = CGRect(x: 0, y: 0, width: height, height: width)
+    engineSlider.center = CGPoint(x: view.frame.width - 120, y: view.frame.height - height/2 - 20)
+    engineSlider.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/2)
+    
+    rudderSlider.frame = CGRect(x: 0, y: 0, width: height, height: width)
+    rudderSlider.center = CGPoint(x: 60, y: view.frame.height - height/2 - 10)
   }
   
-  @objc func rangeSliderValueChanged(_ rangeSlider: RangeSlider) {
-    let values = "(\(rangeSlider.upperValue))"
-    print("Range slider value changed: \(values)")
+   @objc func engineSliderValueChanged(_ rangeSlider: RangeSlider) {
+     let values = "(\(rangeSlider.upperValue))"
+     print("Engine slider value changed: \(values)")
+  }
+   @objc func rudderSliderValueChanged(_ rangeSlider: RangeSlider) {
+        let values = "(\(rangeSlider.upperValue))"
+        print("Rudder slider value changed: \(values)")
+  }
+    
+   @objc func upDownSliderValueChanged(_ rangeSlider: RangeSlider) {
+        let values = "(\(rangeSlider.upperValue))"
+        print("Up down slider value changed: \(values)")
   }
 }
